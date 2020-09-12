581513aae4b51df553c84d60170bcf7f732a592d
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2020-09-12 07:55:26 +0530

    Add files via upload

---
+++ Arrays/Find the Duplicate Number.java
@@ -0,0 +1,19 @@
+/*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one duplicate number in nums, return this duplicate number.*/
+
+// O(n) time and O(n) space using HashSet
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> hset=new HashSet<>();
+        for(int x: nums)
+        {
+            if(!hset.contains(x))
+                hset.add(x);
+            else
+                return x;
+        }
+        return -1;
+    }
+}
+ 
