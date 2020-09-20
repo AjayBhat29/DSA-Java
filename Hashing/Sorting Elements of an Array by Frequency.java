66185ac329365120f7c1049e909fb9d8ff43977f
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2020-09-20 22:21:51 +0530

    Update Sorting Elements of an Array by Frequency.java

--- Hashing/Sorting Elements of an Array by Frequency.java
+++ Hashing/Sorting Elements of an Array by Frequency.java
@@ -1,3 +1,13 @@
+/* Given an array A[] of integers, sort the array according to frequency of elements. That is elements that have higher frequency come first. 
+If frequencies of two elements are same, then smaller number comes first.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. 
+The first line of each test case contains a single integer N denoting the size of array. 
+The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+For each testcase, in a new line, print each sorted array in a seperate line. For each array its numbers should be seperated by space. */
 
 import java.util.*;
 import java.lang.*;
