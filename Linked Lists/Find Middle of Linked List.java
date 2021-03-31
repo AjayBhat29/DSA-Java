09a48e6ae43ea9eddd5682222e96344583e1c403
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-03-31 09:00:28 +0530

    Update Find Middle of Linked List.java

--- Linked Lists/Find Middle of Linked List.java
+++ Linked Lists/Find Middle of Linked List.java
@@ -1,6 +1,25 @@
 /*
-Find Middle of Linked List
+Find Middle Node of Linked List
 */
+
+//Method 1
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode temp=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        int mid=count/2+1;
+        temp=head;
+        while(temp!=null && mid-->1)
+            temp=temp.next;
+        return temp;
+    }
+}
+
+//Method 2
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow=head;
