554e2c97c990bcdb3c76ac0e70f0d77c9d0d0b2f
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-03-31 11:31:51 +0530

    Update Find the Intersection Point of Two LinkedLists.java

--- Linked Lists/Find the Intersection Point of Two LinkedLists.java
+++ Linked Lists/Find the Intersection Point of Two LinkedLists.java
@@ -6,39 +6,90 @@ You may assume there are no cycles anywhere in the entire linked structure.
 Each value on each linked list is in the range [1, 10^9].
 Your code should preferably run in O(n) time and use only O(1) memory. */
 
+//Brute
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode t1=headA;
-        ListNode t2=headB;
-        int len1=0,len2=0;
-        while(t1!=null)
+        if(headA==null || headB==null)return null;
+        ListNode temp1=headA;
+        while(temp1!=null)
         {
-            t1=t1.next;
-            len1++;
+            ListNode temp2=headB;
+            while(temp2!=null)
+            {
+                if(temp2==temp1)
+                    return temp2;
+                temp2=temp2.next;
+            }
+            temp1=temp1.next;
         }
-        while(t2!=null)
-        {
-            t2=t2.next;
-            len2++;
-        }
-        int d=Math.abs(len1-len2);
-        if(len1>len2)
-        {
-            for(int i=0;i<d;i++)
-                headA=headA.next;
-        }
-        else
-        {
-            for(int i=0;i<d;i++)
-                headB=headB.next;
-        }
-        while(headA!=null && headB!=null)
-        {
-            if(headA==headB)
-                return headA;
+        return null;
+    }
+}
+
+//Better
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null)return null;
+        Set<ListNode> hset=new HashSet<>();
+        while(headA!=null){
+            hset.add(headA);
             headA=headA.next;
+        }
+        while(headB!=null){
+            if(hset.contains(headB))
+                return headB;
             headB=headB.next;
         }
         return null;
+    }
+}
+
+//Optimal 1
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null)return null;
+        
+        int n1=0,n2=0;
+        ListNode temp1=headA,temp2=headB;
+        while(temp1!=null){
+            n1++;
+            temp1=temp1.next;
+        }
+        while(temp2!=null){
+            n2++;
+            temp2=temp2.next;
+        }
+        
+        int dif=Math.abs(n1-n2);
+        if(n1>n2)
+            return findCommon(headA,headB,dif);
+        else
+            return findCommon(headB,headA,dif);
+    }
+    ListNode findCommon(ListNode big,ListNode small,int dif)
+    {
+        while(dif-->0)
+            big=big.next;
+        while(big!=small){
+            big=big.next;
+            small=small.next;
+        }
+        return big;
+    }
+}
+
+//Optimal 2
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null)return null;
+        
+        ListNode a=headA;
+        ListNode b=headB;
+        
+        while(a!=b){
+            a=a==null?headB:a.next;
+            b=b==null?headA:b.next;
+        }
+        return a;
     }
 }
