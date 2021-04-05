9d3a259b021d9303b54eac9d2ab12b3bd7fc0b88
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-04-05 18:20:58 +0530

    Add files via upload

---
+++ Sliding Window/Maximum Number of Vowels in a Substring of Given Length.java
@@ -0,0 +1,60 @@
+/* Given a string s and an integer k.
+Return the maximum number of vowel letters in any substring of s with length k.
+Vowel letters in English are (a, e, i, o, u).
+
+Example 1:
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+
+Example 2:
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+
+Example 3:
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+Example 4:
+Input: s = "rhythms", k = 4
+Output: 0
+Explanation: We can see that s doesn't have any vowel letters.
+
+Example 5:
+Input: s = "tryhard", k = 4
+Output: 1
+
+Constraints:
+1 <= s.length <= 10^5
+s consists of lowercase English letters.
+1 <= k <= s.length */
+
+class Solution {
+	public int maxVowels(String s, int k) {
+		int i = 0;
+		int j = k - 1;
+		int count = 0;
+		for (int x = i; x <= j; x++)
+			if (isVowel(s.charAt(x)))
+				count++;
+		int n = s.length();
+		int max = count;
+		while (j < n - 1) {
+			char cur_i = s.charAt(i);
+			char next_j = s.charAt(j + 1);
+			if (isVowel(cur_i))
+				count--;
+			if (isVowel(next_j))
+				count++;
+			max = Math.max(max, count);
+			i++;
+			j++;
+		}
+		return max;
+	}
+	boolean isVowel(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+	}
+}
