612bfb3f6469a18954f25eba0bdc8aba11990b8a
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-04-15 18:37:31 +0530

    Create Max Area of Island.java

---
+++ Graphs/Max Area of Island.java
@@ -0,0 +1,56 @@
+/* Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50 */
+
+class Solution {
+    static int cur,max;
+    public int maxAreaOfIsland(int[][] grid) {
+        max=0;
+        int R=grid.length,C=grid[0].length;
+        for(int i=0;i<R;i++)
+        {
+            for(int j=0;j<C;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    cur=0;
+                    floodFill(grid,i,j,R,C);
+                    max=Math.max(max,cur);
+                }
+            }
+        }
+        return max;
+    }
+    void floodFill(int [][]grid,int i,int j,int R,int C)
+    {
+        if(i<0||i>=R||j<0||j>=C)
+            return;
+        if(grid[i][j]!=1)
+            return;
+        
+        grid[i][j]=0;
+        cur++;
+        
+        floodFill(grid,i+1,j,R,C);
+        floodFill(grid,i-1,j,R,C);
+        floodFill(grid,i,j+1,R,C);
+        floodFill(grid,i,j-1,R,C);
+    }
+}
