49b0b153eb204cf79697b22b212ff74ec5586fce
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-05-29 18:55:49 +0530

    Create House Robber.java

---
+++ Dynamic Programming/House Robber.java
@@ -0,0 +1,42 @@
+/* You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing 
+each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+Constraints:
+1 <= nums.length <= 100
+0 <= nums[i] <= 400 */
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+
+        if (n == 1)
+            return Math.max(0, nums[0]);
+        if (n == 2)
+            return Math.max(0, Math.max(nums[0], nums[1]));
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int include = dp[i - 2] + nums[i];
+            int exclude = dp[i - 1];
+            dp[i] = Math.max(include, exclude);
+        }
+        return dp[n - 1];
+    }
+}
