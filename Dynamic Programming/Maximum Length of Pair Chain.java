f2e33dd677db62f5bf256325eac69b03c97cd3f4
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-05-30 09:34:18 +0530

    Create Maximum Length of Pair Chain.java

---
+++ Dynamic Programming/Maximum Length of Pair Chain.java
@@ -0,0 +1,45 @@
+/* You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+Return the length longest chain which can be formed.
+You do not need to use up all the given intervals. You can select pairs in any order.
+
+Example 1:
+Input: pairs = [[1,2],[2,3],[3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4].
+
+Example 2:
+Input: pairs = [[1,2],[7,8],[4,5]]
+Output: 3
+Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
+ 
+
+Constraints:
+n == pairs.length
+1 <= n <= 1000
+-1000 <= lefti < righti < 1000 */
+
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        int n = pairs.length;
+        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            ans = Math.max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+}
