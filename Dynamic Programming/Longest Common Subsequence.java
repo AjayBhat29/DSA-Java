9ed9b7b0ef817ee65c3d078c20fae2bb1de0cec2
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-06-01 07:27:54 +0530

    Update Longest Common Subsequence.java

--- Dynamic Programming/Longest Common Subsequence.java
+++ Dynamic Programming/Longest Common Subsequence.java
@@ -53,3 +53,22 @@ class Solution {
         return dp[i][j];
     }
 }
+
+//Bottom Up DP
+class Solution {
+    static int lcs(int n1, int n2, String s1, String s2) {
+        int[][] dp = new int[n1 + 1][n2 + 1];
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+
+}
