8a64758c5f34dcd152fc7c6e6c8bda6739f12116
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-06-19 09:32:20 +0530

    Update Longest Increasing Subsequence.java

--- Dynamic Programming/Longest Increasing Subsequence.java
+++ Dynamic Programming/Longest Increasing Subsequence.java
@@ -19,6 +19,7 @@ Constraints:
 1 <= nums.length <= 2500
 -10^4 <= nums[i] <= 10^4 */
 
+// O(n^2) Approach
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
@@ -42,3 +43,35 @@ class Solution {
         return ans;
     }
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+//O(nlog(n)) Approach
+public static int longestIncreasingSubsequence(List < Integer > arr) {
+  // Write your code here
+  List < Integer > dp = new ArrayList < > ();
+  dp.add(arr.get(0));
+  int n = arr.size();
+  for (int i = 1; i < n; i++) {
+    if (dp.get(dp.size() - 1) < arr.get(i)) {
+      dp.add(arr.get(i));
+    } else if (dp.get(dp.size() - 1) > arr.get(i)) {
+      int index = binarySearch(dp, 0, dp.size() - 1, arr.get(i));
+      dp.set(index, arr.get(i));
+    }
+  }
+  return dp.size();
+}
+static int binarySearch(List < Integer > dp, int lo, int hi, int target) {
+  int ans = 0;
+  while (lo <= hi) {
+    int mid = (lo + hi) / 2;
+    if (dp.get(mid) < target)
+      lo = mid + 1;
+    else {
+      ans = mid;
+      hi = mid - 1;
+    }
+  }
+  return ans;
+}
+
