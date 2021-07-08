253991c3baddc76d772654073430bde95087b5dd
Ajay Bhat <ajay18012@cse.ssn.edu.in>
2021-07-08 15:38:29 +0530

    Add files via upload

---
+++ Binary Search/Koko Eating Bananas.java
@@ -0,0 +1,48 @@
+/* Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+Example 1:
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+
+Example 2:
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+Constraints:
+1 <= piles.length <= 10^4
+piles.length <= h <= 10^9
+1 <= piles[i] <= 10^9 */
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int lo = 1, hi = (int) 1e9;
+
+        int ans = 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isPossible(piles, h, mid)) {
+                ans = mid;
+                hi = mid - 1;
+            } else
+                lo = mid + 1;
+        }
+        return ans;
+    }
+
+    boolean isPossible(int[] arr, int H, int speed) {
+        int h = 0;
+        for (int hr : arr)
+            h += Math.ceil((double) hr / speed);
+        return h <= H;
+    }
+}
